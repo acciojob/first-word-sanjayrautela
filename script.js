21241883c564294aa22d6be8bbfae1f89069e058
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-31 10:37:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-function firstWord(s) {
-  // Check if the input string is empty or does not contain any space
-  if (s.trim() === '' || !s.includes(' ')) {
-    return s; // Return the entire string if conditions are met
-  }
-
-  // Extract the first word up to the first space
-  const firstSpaceIndex = s.indexOf(' ');
-  const firstWord = s.substring(0, firstSpaceIndex);
-
-  return firstWord; // Return the first word
+function firstWord(str) {
+    if (!str || !str.includes(' ')) {
+        return str;
+    } else {
+        return str.split(' ')[0];
+    }
 }
 
-// Do not change the code below
-
-const userInput = prompt("Enter String:");
-alert(firstWord(userInput));
